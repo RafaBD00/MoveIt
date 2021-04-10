d94ac02e78d71eeefcf4f5cba8d4484119e7045e
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-10 13:19:07 +0300

    Create LoginActivity.java

---
+++ LoginActivity.java
@@ -0,0 +1,85 @@
+package com.example.movet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+    }
+
+    // Checks if given data is correct and opens main window.
+
+    public void onLogin(View view) {
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        // Encrypts given password with the same encryption that it was encrypted the first time
+        // and checks if they match.
+
+        String testPassword = RegisterActivity.encryptPassword(password);
+
+        if (checkIfMatch(username, testPassword)) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            editTextPassword.setText("");
+            editTextUsername.setText("");
+            Toast toast = Toast.makeText(this,"Username and password doesn't match",
+                    Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        }
+    }
+
+    // Finds the username from the file and compares passwords at that place. If passwords match
+    // returns true and if not returns false.
+
+    public boolean checkIfMatch(String username, String password) {
+        boolean result = false;
+        int i = 1;
+        SharedPreferences sharedPreferences = getSharedPreferences("UserInfo", 0);
+        Map<String, ?> keys = sharedPreferences.getAll();
+        for (Map.Entry<String, ?> value : keys.entrySet()) {
+            if (value.getValue().equals(username)) {
+                String key = value.getKey();
+                String[] split = key.split(":", 2);
+                String rightPassword = sharedPreferences.getString("Password:" + split[1], "");
+                if (rightPassword.equals(password)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    // Opens Registering window for new users.
+
+    public void onSignup(View view) {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
