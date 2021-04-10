c1970ee694bfb6d22b139e0a01c41fb972d34e00
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-10 13:17:50 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.movet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FragmentHome()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentHome()).commit();
+                break;
+            case R.id.nav_notes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentNotes()).commit();
+                break;
+            case R.id.nav_diet:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentDiets()).commit();
+                break;
+            case R.id.nav_workout:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentWorkout()).commit();
+                break;
+            case R.id.nav_progress:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentProgress()).commit();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
