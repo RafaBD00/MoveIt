d784256d13851bfb62025364b49b4f1628d42a36
RafaBD00 <67268787+RafaBD00@users.noreply.github.com>
2021-04-20 12:58:37 +0300

    Update FragmentHome.java

--- FragmentHome.java
+++ FragmentHome.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentHome extends Fragment {
+    private ActiveUserData activeUserData = ActiveUserData.getInstance();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView textHello = v.findViewById(R.id.text3);
+        textHello.setText("Hello " + activeUserData.getUsername() + "!");
+
+        TextView slogan = v.findViewById(R.id.text4);
+        slogan.setText("Strong mind Strong body");
+
+        return (v);
+
     }
+
 }
