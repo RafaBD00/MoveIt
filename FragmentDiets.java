950264f4221b186a3eb5ba669aad84e2f884e22b
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-24 17:29:26 +0300

    Update FragmentDiets.java

--- FragmentDiets.java
+++ FragmentDiets.java
@@ -9,11 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import java.util.Objects;
 
 public class FragmentDiets extends Fragment {
@@ -26,12 +24,16 @@ public class FragmentDiets extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_diets, container, false);
         editTextDiets = (EditText) v.findViewById(R.id.editTextDiets);
+        
         // Put's all the workouts to the editText for the user to read.
+        
         SharedPreferences sharedPreferences = Objects.requireNonNull(getContext()).getSharedPreferences(
                 "DietList", Context.MODE_PRIVATE);
         String work = sharedPreferences.getString("Diet", "Diet");
         editTextDiets.setText(work);
+        
         // Saves the changes made by the user automatically to the file.
+        
         editTextDiets.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
