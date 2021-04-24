1ae80f124044a20a251b2c8c01fba7044b075818
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-24 14:50:11 +0300

    Update ActiveUserData.java

--- ActiveUserData.java
+++ ActiveUserData.java
@@ -12,8 +12,8 @@ import java.util.Map;
 public class ActiveUserData {
     private String name;
     private String username;
-    private String hometown;
     private String allWeights;
+    private String currentUser;
     private double height;
     private double weight;
     private static ActiveUserData instance = null;
@@ -31,18 +31,19 @@ public class ActiveUserData {
     public String getName() {return this.name;}
     public void setUsername(String username) {this.username = username;}
     public String getUsername() {return this.username;}
-    public void setHometown(String hometown) {this.hometown = hometown;}
-    public String getHometown() {return this.hometown;}
     public void setHeight(String height) {this.height = Double.parseDouble(height);}
     public double getHeight() {return this.height;}
     public void setWeight(String weight) {this.weight = Double.parseDouble(weight);}
     public double getWeight() {return this.weight;}
+    public void setCurrentUser(String currentUser) {this.currentUser = currentUser;}
+    public String getCurrentUser() {return this.currentUser;}
 
     // Gets all the different weights the user has given
 
-    public String getAllWeights(Context context, String username) {
+    public String getAllWeights(Context context) {
         SharedPreferences preferences = context.getSharedPreferences("WeightStorage", 0);
-        allWeights = preferences.getString(username, "");
+        String currentWeight = String.valueOf(this.weight);
+        allWeights = preferences.getString(this.username, currentWeight);
         return allWeights;
     }
 
@@ -50,24 +51,15 @@ public class ActiveUserData {
 
     public void addWeight(Context context, String newWeight) {
         this.weight = Double.parseDouble(newWeight);
-        String number = "";
         // Updates users new weight to the UserInfo file in SharedPreference
-        SharedPreferences SPreferences = context.getSharedPreferences("UserInfo", 0);
-        Map<String, ?> keys = SPreferences.getAll();
-        for (Map.Entry<String, ?> value : keys.entrySet()) {
-            if (value.getValue().equals(username)) {
-                String key = value.getKey();
-                String[] split = key.split(":", 2);
-                number = split[1];
-                }
-            }
         SharedPreferences shared = context.getSharedPreferences("UserInfo", 0);
         SharedPreferences.Editor edit = shared.edit();
-        edit.putString("Weight:" + number, newWeight);
+        edit.putString("Weight:" + this.currentUser, newWeight);
         edit.apply();
         // Gets all already added values
         SharedPreferences preferences = context.getSharedPreferences("WeightStorage", 0);
-        String oldWeights = preferences.getString(username, "");
+        String currentWeight = String.valueOf(this.weight);
+        String oldWeights = preferences.getString(this.username, currentWeight);
         // Checks how many weights have been given
         String[] arrWeights = oldWeights.split(";", 21);
         int len = arrWeights.length;
@@ -75,7 +67,7 @@ public class ActiveUserData {
         SharedPreferences sharedPreferences = context.getSharedPreferences("WeightStorage", 0);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         if (oldWeights.equals("")) {
-            editor.putString(username, newWeight);
+            editor.putString(this.username, newWeight);
         } else {
             // Checks if the user has given it's weight 20 times and removes the first so the
             // maximum amount of weights stays at 20 or under
@@ -89,12 +81,18 @@ public class ActiveUserData {
                         oldWeights = oldWeights + ";" + tempWeights[i];
                     }
                 }
-                editor.putString(username, oldWeights + ";" + newWeight);
-            } else {
-                editor.putString(username, oldWeights + ";" + newWeight);
             }
+            editor.putString(this.username, oldWeights + ";" + newWeight);
         }
         editor.apply();
     }
-}
 
+    // Changes the users height
+    public void changeHeight(Context context, String newHeight) {
+        SharedPreferences shared = context.getSharedPreferences("UserInfo", 0);
+        SharedPreferences.Editor edit = shared.edit();
+        edit.putString("Height:" + this.currentUser, newHeight);
+        edit.apply();
+    }
+
+}
