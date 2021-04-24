f33720cc462bbad8d8015370a9a9f0b3d57fd41b
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-24 17:35:51 +0300

    Update FragmentDiets.java

--- FragmentDiets.java
+++ FragmentDiets.java
@@ -1,40 +1,20 @@
-package com.example.movet;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import java.util.Objects;
-
 public class FragmentDiets extends Fragment {
     private EditText editTextDiets;
+    private ActiveUserData activeUserData = ActiveUserData.getInstance();
 
     // This page is a workout page that allows the user to read and update their workouts.
 
     @Nullable
     @Override
-    //This function creates the dietspage where the user can make his/her diets.
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_diets, container, false);
         editTextDiets = (EditText) v.findViewById(R.id.editTextDiets);
-        
         // Put's all the workouts to the editText for the user to read.
-        
         SharedPreferences sharedPreferences = Objects.requireNonNull(getContext()).getSharedPreferences(
                 "DietList", Context.MODE_PRIVATE);
-        String work = sharedPreferences.getString("Diet", "Diet");
+        String work = sharedPreferences.getString(activeUserData.getUsername(), "Diet");
         editTextDiets.setText(work);
-        
         // Saves the changes made by the user automatically to the file.
-        
         editTextDiets.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -48,7 +28,7 @@ public class FragmentDiets extends Fragment {
                 SharedPreferences preferences = Objects.requireNonNull(getContext()).getSharedPreferences(
                         "DietList", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("Diet", workouts);
+                editor.putString(activeUserData.getUsername(), workouts);
                 editor.apply();
             }
         });
