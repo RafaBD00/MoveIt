53f811ee44831813559d04997546b1a578c5ca3b
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-25 14:10:11 +0300

    Update FragmentNotes.java

--- FragmentNotes.java
+++ FragmentNotes.java
@@ -1,32 +1,55 @@
 
 package com.example.movet;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentHome extends Fragment {
-    private ActiveUserData activeUserData = ActiveUserData.getInstance();
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+
+public class FragmentNotes extends Fragment {
+    private final ActiveUserData activeUserData = ActiveUserData.getInstance();
 
     @Nullable
     @Override
-    //This is the notes page where the user can create his/her own notes.
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_home, container, false);
-        TextView textHello = v.findViewById(R.id.text3);
-        textHello.setText("Hello " + activeUserData.getUsername() + "!");
+        View v = inflater.inflate(R.layout.fragment_notes, container, false);
+        TextView textView = v.findViewById(R.id.textView2);
+        textView.setText("Notes");
+        EditText field = v.findViewById(R.id.EditNote);
+        SharedPreferences preference = Objects.requireNonNull(getContext()).getSharedPreferences("SaveNote", Context.MODE_PRIVATE);
+        String text = preference.getString(activeUserData.getUsername(), "Write");
+        field.setText(text);
 
-        TextView slogan = v.findViewById(R.id.text4);
-        slogan.setText("Strong mind Strong body");
 
+        field.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                SharedPreferences settings = Objects.requireNonNull(getContext()).getSharedPreferences("SaveNote", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor;
+                editor = settings.edit();
+                editor.putString(activeUserData.getUsername(), field.getText().toString());
+                editor.apply();
+            }});
         return (v);
-
     }
-
 }
