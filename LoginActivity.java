aa4809915495a651fa3909d497b13aa011dcaccf
Nesssu <80637080+Nesssu@users.noreply.github.com>
2021-04-25 14:24:52 +0300

    Update LoginActivity.java

--- LoginActivity.java
+++ LoginActivity.java
@@ -2,7 +2,6 @@
 package com.example.movet;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,13 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
+
+// This is a login screen. This window opens when the app is opened.
 
 public class LoginActivity extends AppCompatActivity {
     private EditText editTextUsername;
@@ -33,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
     }
 
-    // Checks if given data is correct and opens main window.
+    // Checks if username and password match and opens main window.
 
     public void onLogin(View view) {
         String username = editTextUsername.getText().toString();
@@ -54,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     // Finds the username from the file and compares passwords at that place. If passwords match
-    // returns true and if not returns false.
+    // returns true and if not returns false. If the username and password are a match, it adds the users info to ActiveUserData for later use.
 
     public boolean checkIfMatch(String username, String password) {
         boolean result = false;
